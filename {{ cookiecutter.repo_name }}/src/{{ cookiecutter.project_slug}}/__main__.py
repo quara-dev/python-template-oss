3d5401e8a7a196a334f11680f629395566e2a474
charbonnierg <guillaume.charbonnier@araymond.com>
2023-02-26 09:00:45 +0100

    fix(template): use package name variable instead of hardcoded name

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.project_slug}}/__main__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.project_slug}}/__main__.py
@@ -1,7 +1,7 @@
 {%- if cookiecutter.command_line_interface == "Argparse" -%}
 import sys
 
-from demo_project_argparse.cli.app import main
+from {{ cookiecutter.project_slug }}.cli.app import main
 
 if __name__ == "__main__":
     sys.exit(main())
@@ -9,13 +9,13 @@ if __name__ == "__main__":
 {%- if cookiecutter.command_line_interface == "Click" -%}
 import sys
 
-from demo_project_argparse.cli.app import cli
+from {{ cookiecutter.project_slug }}.cli.app import cli
 
 if __name__ == "__main__":
     sys.exit(cli())
 {%- endif %}
 {%- if cookiecutter.command_line_interface == "Typer" -%}
-from demo_project_argparse.cli.app import cli
+from {{ cookiecutter.project_slug }}.cli.app import cli
 
 if __name__ == "__main__":
     cli()
